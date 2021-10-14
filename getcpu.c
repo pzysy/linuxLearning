e66847f190ce2d1f1ea25c871a80e4c48c5ad6c6
pzysy <20205227070@stu.suda.edu.cn>
2021-10-14 21:02:00 +0800

    sys_getcpu

---
+++ getcpu.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+
+struct getcpu_cache
+{
+        unsigned long blob[128/sizeof(long)];
+};
+
+int getcpu(unsigned *cpu,unsigned *node,struct getcpu_cache *tcache)
+{
+
+        return syscall(SYS_getcpu,cpu,node,tcache);
+}
+
+int main(void)
+{
+        unsigned cpu;
+        unsigned node;
+
+        if(getcpu(&cpu,&node,NULL)==-1)
+        {
+                printf("getcpu bad \n");
+                return 1;
+        }
+
+        printf("cpu = %u node = %u\n",cpu,node);
+
+        return 0;
+}
+
